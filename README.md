4067699709d5f56f052f4b49e202dd930daa01c7
huzhenjie <huzhenjie.dev@gmail.com>
2018-10-31 16:04:41 +0800

    api

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # yinqiantong SDK for Python
+
+## 创建订单
+
+```
+auth.Auth(APP_ID, APP_SECRET).create_order({
+    'channel': '1',
+    'platform': '2',
+    'money': 1,
+    'notify_url': 'https://yinqiantong.com/test',
+    'client_out_trade_no': 'your_clent_out_trade_no',
+    'client_ip': '127.0.0.1',
+})
+```
+
+## 查询订单状态
+
+```
+auth.Auth(APP_ID, APP_SECRET).get_order_state_by_client_out_trade_no(your_client_out_trade_no)
+```
+
+## 生成签名
+
+```
+auth.Auth(APP_ID, APP_SECRET).create_sign({
+    'channel': '1',
+    'platform': '2',
+    'money': 1,
+    'client_ip': '127.0.0.1'
+})
+```
+
+## 校验签名
+
+```
+auth.Auth(APP_ID, APP_SECRET).check_sign(your_data)
+```
--- yinqiantong/auth.py
+++ yinqiantong/auth.py
@@ -1,23 +1,100 @@
 # -*- coding: utf-8 -*-
-
 import time
+import json
+import urllib2
 import hashlib
 
 
+def http_post_json(url, data=None, headers=None):
+    if headers is None:
+        headers = {}
+    if data is None:
+        data = {}
+    try:
+        req = urllib2.Request(url)
+        req.add_header('Content-Type', 'application/json')
+        for k, v in headers.items():
+            req.add_header(k, v)
+        response = urllib2.urlopen(req, json.dumps(data))
+        return response.code, response.read()
+    except urllib2.HTTPError, e:
+        return e.code, e
+    except urllib2.URLError, e:
+        return 0, e
+    except Exception as e:
+        return 0, e
+
+
+def http_get(url, headers=None):
+    if headers is None:
+        headers = {}
+    try:
+        req = urllib2.Request(url)
+        for k, v in headers.items():
+            req.add_header(k, v)
+        response = urllib2.urlopen(req)
+        return response.code, response.read()
+    except urllib2.HTTPError, e:
+        return e.code, e
+    except urllib2.URLError, e:
+        return 0, e
+    except Exception as e:
+        return 0, e
+
+
 class Auth(object):
-    def __init__(self, app_id, app_key, app_secret):
+    def __init__(self, app_id, app_secret):
         self.__app_id = app_id
-        self.__app_key = app_key
         self.__app_secret = app_secret
 
-    def create_sign(self, data=None, ts=None):
-        if not ts:
-            ts = long(time.time())
+    def create_sign(self, data=None):
+        if data is None:
+            data = {}
+        if not data.get('ts'):
+            data['ts'] = long(time.time())
+        if not data.get('appid'):
+            data['appid'] = self.__app_id
         origin_sign_str = ''
-        if data:
-            keys = sorted(data.keys())
-            for k in keys:
-                v = data.get(k)
-                origin_sign_str += '%s=%s&' % (k, v)
-        origin_sign_str += 'ts=%s&app_secret=%s' % (ts, self.__app_secret)
+        keys = sorted(data.keys())
+        for k in keys:
+            v = data.get(k)
+            if not v or v == '' or k == 'sign' or k == '':
+                continue
+            origin_sign_str += '%s=%s&' % (k, v)
+        origin_sign_str += 'app_secret=%s' % (self.__app_secret)
         return hashlib.md5(origin_sign_str).hexdigest()
+
+    def check_sign(self, data):
+        if not data or not data.get('sign'):
+            return False
+        curr_sign = self.create_sign(data)
+        return curr_sign == data.get('sign')
+
+    def create_order(self, data):
+        if data is None:
+            data = {}
+        if not data.get('ts'):
+            data['ts'] = long(time.time())
+        if not data.get('appid'):
+            data['appid'] = self.__app_id
+        sign = self.create_sign(data)
+        data['sign'] = sign
+        (code, data_str) = http_post_json('https://yqtapi.com/order', data)
+        res_data = json.loads(data_str)
+        if res_data and res_data['data'] and res_data['code'] == 200:
+            data['pay_body'] = res_data['data']['pay_body']
+            data['out_trade_no'] = res_data['data']['out_trade_no']
+            data['expire_time'] = res_data['data']['expire_time']
+            res_data['data'] = data
+        return res_data
+
+    def get_order_state_by_out_trade_no(self, out_trade_no):
+        (code, data_str) = http_get('https://yqtapi.com/order?appid=%s&out_trade_no=%s' % (self.__app_id, out_trade_no))
+        res_data = json.loads(data_str)
+        return res_data
+
+    def get_order_state_by_client_out_trade_no(self, client_out_trade_no):
+        (code, data_str) = http_get(
+            'https://yqtapi.com/order?appid=%s&client_out_trade_no=%s' % (self.__app_id, client_out_trade_no))
+        res_data = json.loads(data_str)
+        return res_data
